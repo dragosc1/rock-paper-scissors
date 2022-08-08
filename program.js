1b833b89fff8567bbd6dd8e107bae1d8e9c56f83
dragosc1 <dragos.ciobanu232@gmail.com>
2022-08-08 20:31:30 +0300

    Added a playRound function

--- program.js
+++ program.js
@@ -6,3 +6,25 @@ function getComputerChoice() {
         return "paper";
     else return "scissors";
 }
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection == "rock")
+        if (computerSelection == "rock")
+            return "Draw";
+        else if (computerSelection == "paper")
+            return "You Lost! Paper beats Rock";
+        else return "You Won! Rock beats Scissors";
+    else if (playerSelection == "paper")
+        if (computerSelection == "rock")
+            return "You Won! Paper beats Rock";
+        else if (computerSelection == "paper")
+            return "Draw!";
+        else return "You Lost! Scissors beats Paper";
+    else
+        if (computerSelection == "rock")
+            return "You Lost! Rock beats Scissors";
+        else if (computerSelection == "paper")
+            return "You Won! Scissors beats Paper";
+        else return "Draw!";
+}
+console.log(playRound("rock", "rock"));
